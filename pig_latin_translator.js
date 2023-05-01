a0f3387be7b5b85808ff72a348fa4f58774efb5c
mattyu123 <matthewyu100@gmail.com>
2023-05-01 15:24:21 -0600

    added new files in music_library assignment

--- pig_latin_translator.js
+++ pig_latin_translator.js
@@ -1,15 +1,15 @@
-// var originalWords = process.argv.slice(2);
-// var pigLatinWords = [];
+const originalWords = process.argv.slice(2);
+let pigLatinWords = [];
 
-// for (var i = 0; i < originalWords.length; i++) {
-//   pigLatinWords.push(translateToPigLatin(originalWords[i]));
-// }
+for (let i = 0; i < originalWords.length; i++) {
+  pigLatinWords.push(translateToPigLatin(originalWords[i]));
+}
 
-// console.log(pigLatinWords.join(' '));
+console.log(pigLatinWords.join(' '));
 
-// function translateToPigLatin(word) {
-//   return word.slice(1, word.length) + word[0] + "ay";
-// }
+function translateToPigLatin(word) {
+  return word.slice(1, word.length) + word[0] + "ay";
+}
 
-// console.log(translateToPigLatin("pig latin is silly"));
+console.log(translateToPigLatin("pig latin is silly"));
 
